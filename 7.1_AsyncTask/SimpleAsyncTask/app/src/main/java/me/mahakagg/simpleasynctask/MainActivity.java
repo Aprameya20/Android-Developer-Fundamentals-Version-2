85c94b18a81f8a43a089e7da741ea8b1d38f4932
Ananya Aprameya <ananyaaprameya5@gmail.com>
2021-12-06 09:31:34 +0530

    asdf

--- 7.1_AsyncTask/SimpleAsyncTask/app/src/main/java/me/mahakagg/simpleasynctask/MainActivity.java
+++ 7.1_AsyncTask/SimpleAsyncTask/app/src/main/java/me/mahakagg/simpleasynctask/MainActivity.java
@@ -1,4 +1,4 @@
-package me.mahakagg.simpleasynctask;
+package me.mahakagg.simpleasynctaskcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,28 +8,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private TextView mProgressTextView;
     private static final String TEXT_STATE = "CurrentText";
+    private static final String TEXT_PROGRESS = "CurrentSleepDuration";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.textView1);
+        mProgressTextView = findViewById(R.id.textViewResult);
 
         if (savedInstanceState != null){
             mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+            mProgressTextView.setText(savedInstanceState.getString(TEXT_PROGRESS));
         }
     }
 
     public void startTask(View view) {
         mTextView.setText(R.string.napping);
-        new SimpleAsyncTask(mTextView).execute();
+        new SimpleAsyncTask(mTextView, mProgressTextView).execute();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(TEXT_STATE,
-                mTextView.getText().toString());
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+        outState.putString(TEXT_PROGRESS, mProgressTextView.getText().toString());
     }
 }
--- 7.1_AsyncTask/SimpleAsyncTask/app/src/main/java/me/mahakagg/simpleasynctask/SimpleAsyncTask.java
+++ 7.1_AsyncTask/SimpleAsyncTask/app/src/main/java/me/mahakagg/simpleasynctask/SimpleAsyncTask.java
@@ -1,4 +1,4 @@
-package me.mahakagg.simpleasynctask;
+package me.mahakagg.simpleasynctaskcc;
 
 import android.os.AsyncTask;
 import android.widget.TextView;
@@ -7,13 +7,15 @@ import java.lang.ref.WeakReference;
 import java.util.Random;
 
 // first void because no input required; second void because progress is not published; string for result type
-public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
 
    // The weak reference prevents the memory leak by allowing the object held by that reference to be garbage collected if necessary.
     private WeakReference<TextView> mTextView;
+    private WeakReference<TextView> mResultTextView;
 
-    SimpleAsyncTask (TextView tv){
+    SimpleAsyncTask (TextView tv, TextView result){
         mTextView = new WeakReference<>(tv);
+        mResultTextView = new WeakReference<>(result);
     }
 
     @Override
@@ -21,6 +23,7 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
         Random random = new Random();
         int number = random.nextInt(11);
         int milli = number * 200;
+        publishProgress(milli);
         try {
             Thread.sleep(milli);
         }
@@ -28,6 +31,11 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
             e.printStackTrace();
         }
         return "Awake after sleeping for " + milli + " milliseconds";
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        mResultTextView.get().setText("Current sleep time: " + values[0] + " ms");
     }
 
     @Override
